12e7e66f3f5f736937566a45c6cc41c5fdf27339
Evans Yeung <evansbyeung@gmail.com>
2021-02-26 16:05:32 -0500

    Add view  modifiers

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -12,12 +12,24 @@ struct ContentView: View {
         VStack {
             VStack {
                 Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    // Instead of hard coding a font size, use built-in dynamic sizing
+                    .font(.footnote)
                 Text("89")
+                    .kerning(1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
             }
             HStack {
                 Text("1")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                // Default value to 50, in range 1 to 100
                 Slider(value: .constant(50), in: 1.0...100.0)
                 Text("100")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             }
             Button(action: {}) {
                 Text("Hit Me")
